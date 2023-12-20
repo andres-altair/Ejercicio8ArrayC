d9a6791e69228fd8f7fc73f9d66a74d827ef1cad
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-12-20 11:38:12 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Xml.Linq;
+
+namespace Ejercicio8ArrayC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INDEXOFF
+            string[] palabras = { "Hola", "Adiós", "Nuevo", "Viejo", "Vamos" };
+            string ele = "Adiós";
+            int indice = Array.IndexOf(palabras, ele);
+            bool continuar = false;
+
+            if (indice >= 0)
+            {
+                Console.WriteLine($"Elemento {ele} encontrado en el índice: {indice}");
+            }
+            else
+            {
+                Console.WriteLine($"Elemento {ele} no encontrado.");
+            }
+            
+            
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------------");
+            
+            
+            do {Console.WriteLine("escribe la palabra");
+            string palusu = Console.ReadLine(); 
+            int ind =Array.IndexOf(palabras, palusu);
+            if (ind >= 0) 
+            {
+                Console.WriteLine($"Elemento {palusu} encontrado en el índice: {indice}");
+            }
+            else
+            {
+                Console.WriteLine($"Elemento  {palusu} no encontrado.");
+            }
+            Console.WriteLine("1. continuar , 2. para terminar");
+               int seguir = Convert.ToInt32( Console.ReadLine());
+                if (seguir == 1)
+                {
+                    continuar = false;
+                }
+                else { continuar = true; }
+            }
+            while (!continuar);
+
+            //WHERE - filtrar
+            int[] numeros = { 1, 5, 7, 8, 4, 3, 9 };
+            numeros.Where(numeros=>numeros>3);
+           
+        }
+    }
+}
